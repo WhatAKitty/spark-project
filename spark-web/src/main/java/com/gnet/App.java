a11549908730141057cf0eb88a7c091a994becd6
WhatAKitty <104xuqiang@163.com>
2017-01-20 15:10:45 +0800

    init commit

---
+++ spark-web/src/main/java/com/gnet/App.java
@@ -0,0 +1,13 @@
+package com.gnet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
